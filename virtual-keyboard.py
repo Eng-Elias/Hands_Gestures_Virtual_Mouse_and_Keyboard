a7a0c91481c09b177a9fae3246dba04337203185
Eng. Elias <eng.elias.owis@gmail.com>
2024-12-03 23:30:30 +0400

    add more keys on the virtual keyboard with ability to insert symbols and activate some control buttons like Win and ESC

--- virtual-keyboard.py
+++ virtual-keyboard.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-from pynput.keyboard import Controller
+from pynput.keyboard import Controller, Key
 import time
 import math
 import pyautogui
@@ -19,71 +19,190 @@ class VirtualKeyboard:
         )
         self.mp_draw = mp.solutions.drawing_utils
         
-        # Define the keyboard layout
-        self.keys = [
-            ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-            ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';'],
-            ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/']
-        ]
+        # Define the keyboard layout with normal and shift states
+        self.keys = {
+            'normal': [
+                ['Esc', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'],
+                ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+                ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\'],
+                ['Caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter'],
+                ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'Shift'],
+                ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl']
+            ],
+            'shift': [
+                ['Esc', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'],
+                ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'],
+                ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|'],
+                ['Caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'],
+                ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Shift'],
+                ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl']
+            ]
+        }
         
-        # Button properties - smaller size
-        self.button_width = 30
-        self.button_height = 30
-        self.button_margin = 3
+        # Special keys mapping
+        self.special_keys = {
+            'Space': Key.space,
+            'Tab': Key.tab,
+            'Enter': Key.enter,
+            'Backspace': Key.backspace,
+            'Esc': Key.esc,
+            'Win': Key.cmd,
+            'F1': Key.f1,
+            'F2': Key.f2,
+            'F3': Key.f3,
+            'F4': Key.f4,
+            'F5': Key.f5,
+            'F6': Key.f6,
+            'F7': Key.f7,
+            'F8': Key.f8,
+            'F9': Key.f9,
+            'F10': Key.f10,
+            'F11': Key.f11,
+            'F12': Key.f12
+        }
+        
+        # Control key states
+        self.shift_pressed = False
+        self.caps_lock = False
+        self.ctrl_pressed = False
+        self.alt_pressed = False
+        
+        # Button properties
+        self.button_width = 40
+        self.button_height = 40
+        self.button_margin = 5
+        
+        # Special key widths
+        self.key_widths = {
+            'Backspace': 80,
+            'Tab': 60,
+            'Caps': 80,
+            'Enter': 80,
+            'Shift': 100,
+            'Ctrl': 60,
+            'Alt': 60,
+            'Space': 240,
+            'Win': 60
+        }
         
         # Window properties
-        self.window_width = 400
-        self.window_height = 200
+        self.window_width = 1000
+        self.window_height = 400
         
         # Clicking properties
         self.clicked = False
-        self.click_cooldown = 0.5  # seconds
+        self.click_cooldown = 0.2  # seconds
         self.last_click_time = 0
+    
+    def get_key_width(self, key):
+        """Get the width of a specific key"""
+        return self.key_widths.get(key, self.button_width)
     
     def draw_keyboard(self, img):
         keyboard_start_x = 20
         keyboard_start_y = 20
+        current_y = keyboard_start_y
         
-        for row_idx, row in enumerate(self.keys):
-            for col_idx, key in enumerate(row):
-                x = keyboard_start_x + col_idx * (self.button_width + self.button_margin)
-                y = keyboard_start_y + row_idx * (self.button_height + self.button_margin)
+        # Choose layout based on shift state
+        layout = self.keys['shift' if self.shift_pressed else 'normal']
+        
+        for row in layout:
+            current_x = keyboard_start_x
+            max_height = self.button_height
+            
+            for key in row:
+                width = self.get_key_width(key)
                 
-                # Create a semi-transparent black background
-                overlay = img.copy()
-                cv2.rectangle(overlay, (x, y), 
-                            (x + self.button_width, y + self.button_height), 
-                            (0, 0, 0), -1)  # Filled black rectangle
-                alpha = 0.5  # Transparency factor
-                cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0, img)
+                # Draw button background
+                cv2.rectangle(img, (current_x, current_y), 
+                            (current_x + width, current_y + self.button_height), 
+                            (50, 50, 50), -1)  # Filled dark gray background
                 
                 # Draw button border
-                cv2.rectangle(img, (x, y), 
-                            (x + self.button_width, y + self.button_height), 
-                            (255, 255, 255), 2)
+                border_color = (0, 255, 0) if key in ['Shift', 'Caps'] and self.shift_pressed else (255, 255, 255)
+                cv2.rectangle(img, (current_x, current_y), 
+                            (current_x + width, current_y + self.button_height), 
+                            border_color, 2)
                 
                 # Draw text
                 text_size = cv2.getTextSize(key, cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
-                text_x = x + (self.button_width - text_size[0]) // 2
-                text_y = y + (self.button_height + text_size[1]) // 2
+                text_x = current_x + (width - text_size[0]) // 2
+                text_y = current_y + (self.button_height + text_size[1]) // 2
                 cv2.putText(img, key, (text_x, text_y), 
                            cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
                 
+                current_x += width + self.button_margin
+
+            current_y += self.button_height + self.button_margin
+
     def get_clicked_key(self, finger_pos):
         keyboard_start_x = 20
         keyboard_start_y = 20
+        current_y = keyboard_start_y
+        
+        # Choose layout based on shift state
+        layout = self.keys['shift' if self.shift_pressed else 'normal']
         
         x, y = finger_pos
         
-        for row_idx, row in enumerate(self.keys):
-            for col_idx, key in enumerate(row):
-                button_x = keyboard_start_x + col_idx * (self.button_width + self.button_margin)
-                button_y = keyboard_start_y + row_idx * (self.button_height + self.button_margin)
+        for row in layout:
+            current_x = keyboard_start_x
+            
+            for key in row:
+                width = self.get_key_width(key)
                 
-                if (button_x < x < button_x + self.button_width and 
-                    button_y < y < button_y + self.button_height):
+                if (current_x < x < current_x + width and 
+                    current_y < y < current_y + self.button_height):
                     return key
+                
+                current_x += width + self.button_margin
+            
+            current_y += self.button_height + self.button_margin
+        
         return None
+    
+    def handle_key_press(self, key):
+        """Handle key press with special key functionality"""
+        if key is None:
+            return
+            
+        if key == 'Shift':
+            self.shift_pressed = not self.shift_pressed
+        elif key == 'Caps':
+            self.caps_lock = not self.caps_lock
+        elif key == 'Ctrl':
+            self.ctrl_pressed = not self.ctrl_pressed
+        elif key == 'Alt':
+            self.alt_pressed = not self.alt_pressed
+        elif key in self.special_keys:
+            # Handle special keys using pynput Key enum
+            special_key = self.special_keys[key]
+            self.keyboard.press(special_key)
+            self.keyboard.release(special_key)
+        else:
+            # Handle regular keys
+            char = key
+            if len(key) == 1:  # Only process single characters
+                if self.caps_lock != self.shift_pressed:  # XOR
+                    char = key.upper()
+                else:
+                    char = key.lower()
+            
+            # Handle Ctrl combinations
+            if self.ctrl_pressed:
+                if key.lower() in ['c', 'v', 'x', 'z', 'a']:
+                    with self.keyboard.pressed(Key.ctrl):
+                        self.keyboard.press(key.lower())
+                        self.keyboard.release(key.lower())
+                return
+            
+            # Press the key
+            self.keyboard.press(char)
+            self.keyboard.release(char)
+            
+            # Reset shift if it was pressed
+            if self.shift_pressed and key != 'Shift':
+                self.shift_pressed = False
     
     def detect_click(self, hand_landmarks):
         # Get index finger tip and thumb tip positions
@@ -110,8 +229,9 @@ class VirtualKeyboard:
         window_x = (screen_width - self.window_width) // 2
         window_y = screen_height - self.window_height - 40  # 40 pixels from bottom
         
-        # Create and position control window
-        cv2.namedWindow("Virtual Keyboard")
+        # Create and position control window with always on top property
+        cv2.namedWindow("Virtual Keyboard", cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_EXPANDED)
+        cv2.setWindowProperty("Virtual Keyboard", cv2.WND_PROP_TOPMOST, 1)
         cv2.moveWindow("Virtual Keyboard", window_x, window_y)
         
         prev_clicked = False
@@ -155,8 +275,7 @@ class VirtualKeyboard:
                 if is_clicked and not prev_clicked and current_time - self.last_click_time > self.click_cooldown:
                     clicked_key = self.get_clicked_key((finger_x, finger_y))
                     if clicked_key:
-                        self.keyboard.press(clicked_key.lower())
-                        self.keyboard.release(clicked_key.lower())
+                        self.handle_key_press(clicked_key)
                         print(f"Pressed: {clicked_key}")
                         self.last_click_time = current_time
                         # Visual feedback for key press
